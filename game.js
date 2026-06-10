3215c99f3ebd9bec2b6d9291fabbbfc13dfb1e27
Talonmom-100 <lil_chanti50@hotmail.com>
2026-06-10 14:22:30 -0600

    Let's see if this works!
    Hopefully it will...

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ const { LootTable } = lootalot
 var loot
 var raritything
 var raritydisplay
+var rollcount = 1
 
 const rtable = new LootTable(
     [
@@ -12,6 +13,16 @@ const rtable = new LootTable(
 )
 
 function roll(x) {
+    raritydisplay = ""
     loot = rtable.loot(x)
     console.table(loot)
+    for (let i = 0; i < (x - 1); i++) {
+        raritything = loot[i][0] + " x" + loot[i][1]
+        if (i < 1) {
+            raritydisplay = raritything
+        } else {
+            raritydisplay = raritydisplay + ", " + raritything
+        }
+    }
+    document.getElementById("raritydisplaytext").innerHTML = "You got: " + raritydisplay
 }
